90c1cee28ae88fc2798299b937cb819fc51a7d78
diptesh03 <diptesh03@gmail.com>
2018-01-06 15:47:46 +0530

    Database Update

---
+++ PanchayatDB/dbo/Tables/Notice.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Notice]
+(
+	[NoticeID] INT NOT NULL PRIMARY KEY, 
+    [Subject] VARCHAR(200) NULL, 
+    [Body] VARCHAR(MAX) NULL, 
+    [To] VARCHAR(100) NULL, 
+    [ToAddress] VARCHAR(200) NULL, 
+    [CopyTo] VARCHAR(100) NULL, 
+    [CopyToAddress] VARCHAR(100) NULL, 
+    [UserID] NVARCHAR(128) NULL, 
+    CONSTRAINT [FK_Notice_AspNetUsers] FOREIGN KEY ([UserID]) REFERENCES [AspNetUsers]([Id])
+)
